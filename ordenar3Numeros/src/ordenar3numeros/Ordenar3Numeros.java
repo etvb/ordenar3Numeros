ca7237f19e209f3a5f68e20481b3f538ccaa755b
Edgardo Torres Vanden Bouch <etvb66@hotmail.com>
2017-09-22 06:27:18 -0500

    pedimos 3 numeros y usamos la logica de if else if y un if afuera

---
+++ ordenar3Numeros/src/ordenar3numeros/Ordenar3Numeros.java
@@ -0,0 +1,53 @@
+//Ejercicio7: Pedir 3 numeros y mostrarlos ordenados de mayor a menor.
+
+package ordenar3numeros;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author edgardo
+ */
+public class Ordenar3Numeros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        short num1, num2, num3;
+        
+        num1 = Short.parseShort(JOptionPane.showInputDialog("Ingresa el PRIMER número"));
+        num2 = Short.parseShort(JOptionPane.showInputDialog("Ingresa el SEGUNDO número"));
+        num3 = Short.parseShort(JOptionPane.showInputDialog("Ingresa el TERCER número"));
+        
+        //cuando usamos if else if, si cualquiera de ellos es verdadero, omite los demas que siguen.
+        //Si usuamos if separados, evalua cada if 
+        if(num1 > num2) {
+            if(num1 > num3){
+               if(num2 > num3){
+                   JOptionPane.showMessageDialog(null, num1 +" "+ num2 +" "+ num3);
+               } else{
+                   JOptionPane.showMessageDialog(null, num1 +" "+ num3 +" "+ num2);
+               }
+            }
+        }else if(num2 > num3){
+            if(num1 > num3) {
+                JOptionPane.showMessageDialog(null, num2+" "+num1+" "+num3);
+            }else{
+                JOptionPane.showMessageDialog(null, num2+" "+num3+" "+num1);
+            }
+         }
+        // Lo sacamos del else if, por que cuando era 2,1,3 evaluaba el primero y nunca entraba a este. 
+        if(num3 > num1){
+             if(num1 > num2){
+                 JOptionPane.showMessageDialog(null, num3+" "+num1+" "+num2);
+             }else
+                 JOptionPane.showMessageDialog(null, num3+" "+num2+" "+num1);
+             
+         }
+            
+        
+    }
+    
+}
